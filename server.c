37478cc686a6199bb9b06a6f5d129183db38215c
Moritz Schmidt <mo.schmidt1007@gmail.com>
2026-09-11 20:38:41 +0200

    Added robustness with more 'perror' handling

--- server.c
+++ server.c
@@ -8,7 +8,7 @@
 
 typedef struct {
     char *ip_address;
-    size_t port;
+    uint16_t port;
 } binding_args;
 
 int establish_connection(){
@@ -21,24 +21,28 @@ int establish_connection(){
     return sock;
 }
 
-struct sockaddr_in bind_socket_base(int socket_fd, char *ip_addr, size_t port){
-    struct sockaddr_in address;
+struct sockaddr_in bind_socket_base(int socket_fd, char *ip_addr, uint16_t port){
+    struct sockaddr_in address = {0};
+
     address.sin_family = AF_INET;
-    address.sin_addr.s_addr = inet_addr(ip_addr);
+    if(inet_pton(AF_INET, ip_addr, &address.sin_addr) < 0){
+        perror("inet_pton");
+        return (struct sockaddr_in){0};
+    }
     address.sin_port = htons(port);
     
     if(bind(socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0){
         perror("Socket-Binding-Fehler!");
         return (struct sockaddr_in){0};
     }
-    printf("Socket erfolgreich festgelegt auf %s:%zu\n", ip_addr, port);
+    printf("Socket erfolgreich festgelegt auf %s:%hu\n", ip_addr, port);
     return address;    
 }
 
 struct sockaddr_in bind_socket_variadic(int socket_fd, binding_args args){
     char *ip_add_out = args.ip_address ? args.ip_address : "127.0.0.1";
 
-    size_t port_out = args.port ? args.port : 8080;
+    uint16_t port_out = args.port ? args.port : 8080;
 
     return bind_socket_base(socket_fd, ip_add_out, port_out);
 }
@@ -51,10 +55,7 @@ void terminate_connection(int *sock){
 } 
 
 void event_loop(int sock, struct sockaddr_in address){
-    listen(sock,3);
-
     while(1){
-
         socklen_t addrlen = sizeof(address);
         int new_socket = accept(sock, (struct sockaddr *)&address, &addrlen);
         
@@ -66,26 +67,39 @@ void event_loop(int sock, struct sockaddr_in address){
         printf("Connection accepted. \n");
 
         char buffer[1024] = {0};
-        read(new_socket, buffer, sizeof(buffer));
+        ssize_t bytes_read = read(new_socket, buffer, sizeof(buffer) - 1);
+
+        if(bytes_read < 1){
+            perror("read");
+            terminate_connection(&new_socket);
+            continue;
+        }
+        buffer[bytes_read] = '\0';
+
         printf("Client Message : %s \n", buffer);
         
         char *response = "Hello from Server!";
-        write(new_socket, response, strlen(response));
+        
+        if(write(new_socket, response, strlen(response))){
+            perror("");
+            continue;
+        }
 
         terminate_connection(&new_socket);
     }
 }
 
 int main(int argc, char *argv[]){
-    int i = 15;
     int sock = establish_connection();
     if(sock < 0){
         return EXIT_FAILURE;
     };
     struct sockaddr_in bound_socket = bind_socket(sock);
-    
-    listen(sock, 3);
-
+   
+    if( listen(sock, 3) < 0){
+        perror("listen");
+        return EXIT_FAILURE;
+    }
     event_loop(sock, bound_socket);
 
     terminate_connection(&sock);
