353595f8e9cc7f1bdd7a2ddbf632f490673aa09b
Moritz Schmidt <mo.schmidt1007@gmail.com>
2026-09-13 17:07:48 +0200

    rewritten server-logik

--- server.c
+++ server.c
@@ -5,133 +5,107 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<string.h>
-#include<errno.h>
 
 typedef struct {
     char *ip_address;
     uint16_t port;
 } binding_args;
 
-
-struct sockaddr_in create_sockaddr(char *ip_addr, uint16_t port){
-    struct sockaddr_in address = {0};
-
-    address.sin_family = AF_INET;
-
-    int result = inet_pton(address.sin_family, ip_addr, &address.sin_addr);
-    if(result != 1){
-        if (result < 0){
-            perror("inet_pton");
-        }
-        else{
-            fprintf(stderr, "Invalid IP-Address %s \n", ip_addr);
-        }
-        return (struct sockaddr_in){0};
-    }
-    address.sin_port = htons(port);
-
-    return address;
-}
+#define PORT 7890
 
 int create_socket(){
-    int sock = socket(AF_INET, SOCK_STREAM, 0); 
+    int sock = socket(PF_INET, SOCK_STREAM, 0); 
     if(sock < 0){
         perror("create_socket");
         return -1;
     }
-    printf("socket created successfully\n");
     return sock;
 }
 
-struct sockaddr_in bind_socket_base(int socket_fd, char *ip_addr, uint16_t port){
-    struct sockaddr_in address = create_sockaddr(ip_addr, port);
-    if(bind(socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0){
-        perror("bind_socket_base");
-        return (struct sockaddr_in){0};
+void set_socket_option(int sock){
+    int yes = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0){ 
+        perror("Setting-sock-Option");
+        return;
     }
-    printf("socket bound to %s:%hu\n", ip_addr, port);
-    return address;    
 }
 
-struct sockaddr_in bind_socket_variadic(int socket_fd, binding_args args){
-    char *ip_add_out = args.ip_address ? args.ip_address : "127.0.0.1";
-    uint16_t port_out = args.port ? args.port : 8080;
-
-    return bind_socket_base(socket_fd, ip_add_out, port_out);
+void set_own_addr_information(struct sockaddr_in *addr){
+    addr->sin_family = AF_INET;
+    addr->sin_port = htons(PORT);
+    addr->sin_addr.s_addr = 0;
+    memset(&(addr->sin_zero), '\0', 8);
 }
 
-#define bind_socket(socket_fd, ...) bind_socket_variadic(socket_fd, (binding_args) {__VA_ARGS__})
+void dump(const unsigned char *data, const unsigned int length){
+    unsigned char byte;
+    unsigned int i, j;
+    for(i = 0; i < length; i++){
+        byte = data[i];
+        printf("%02x", data[i]); // byte in hexadecimal
+        if((i%16)==15 || i ==length - 1){
+            for(j=0; j < 15-(i%16);j++){
+                printf(" ");
+            }
+            printf("| ");
+            for(j=i-(i%16); j<= i; j++){
+                byte = data[j];
+                if(byte > 31 && byte < 127){
+                    printf("%c", byte);
+                }
+                else{
+                    printf(".");
+                }
+            }
+            printf("\n");
+        }
+    }
+}
 
 void event_loop(int sock){
+    socklen_t sin_size;
+    struct sockaddr_in client_addr;
+    int new_sock, recv_length;
+    char buffer[1024] = {0};
     while (1) {
-        struct sockaddr_in client_address;
-        socklen_t addrlen = sizeof(client_address);
-
-        int new_socket = accept(
-            sock,
-            (struct sockaddr *)&client_address,
-            &addrlen
-        );
-
-        if (new_socket < 0) {
+        sin_size = sizeof(struct sockaddr_in);
+        new_sock = accept(sock, (struct sockaddr_in *)&client_addr, &sin_size); 
+        if(new_sock < 0){
             perror("accept");
+            close(new_sock);
             continue;
         }
 
-        printf("Connection accepted.\n");
-
-        char buffer[1024] = {0};
-
-        ssize_t bytes_read = read(
-            new_socket,
-            buffer,
-            sizeof(buffer) - 1
-        );
-
-        if(bytes_read < 1){
-            perror("reading");
-            close(new_socket);
-            continue;
+        printf("server: new connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+        send(new_sock,  "Hello, there!\n", 13, 0);
+        recv_length = recv(new_sock, &buffer, 1024, 0);
+        while(recv_length > 0){
+            printf("RECV: %d bytes\n", recv_length);
+            dump(buffer, recv_length);
+            recv_length = recv(new_sock, &buffer, 1024, 0);
         }
-
-        if(bytes_read == 0){
-            printf("Client closed connection.");
-            close(new_socket);
-            continue;
-        }
-
-        buffer[bytes_read] = '\0';
-
-        printf("Client Message: %s\n", buffer);
-
-        const char *response = "Hello from Server!";
-
-        ssize_t bytes_written = write(
-            new_socket,
-            response,
-            strlen(response)
-        );
-
-        if (bytes_written < 0) {
-            perror("write");
-        }
-
-        close(new_socket);
+        close(new_sock);
     }
 }
 
-int main(int argc, char *argv[]){
+int main(void){
     int sock = create_socket();
-    if(sock < 0){
+    struct sockaddr_in host_addr, client_addr;
+    set_socket_option(sock);
+    set_own_addr_information(&host_addr);
+
+    if(bind(sock, (struct sockaddr_in *)host_addr, sizeof(struct sockaddr)) < 0){
+        perror("bind");
+        close(sock);
         return EXIT_FAILURE;
-    };
-    struct sockaddr_in bound_socket = bind_socket(sock);
-   
+    }
+    
     if( listen(sock, 3) < 0){
         perror("listen");
         close(sock);
         return EXIT_FAILURE;
     }
+
     event_loop(sock);
 
     close(sock);
