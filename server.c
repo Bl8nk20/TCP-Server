536cbe81b60b9dd14e6ed665321b76fb9848820c
Moritz Schmidt <mo.schmidt1007@gmail.com>
2026-09-13 17:17:24 +0200

    rewritten server-logic

--- server.c
+++ server.c
@@ -13,16 +13,15 @@ typedef struct {
 
 #define PORT 7890
 
-int create_socket(){
-    int sock = socket(PF_INET, SOCK_STREAM, 0); 
+void create_socket(int *sock){
+    sock = socket(PF_INET, SOCK_STREAM, 0); 
     if(sock < 0){
         perror("create_socket");
-        return -1;
+        return;
     }
-    return sock;
 }
 
-void set_socket_option(int sock){
+void set_socket_option(int *sock){
     int yes = 1;
     if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0){ 
         perror("Setting-sock-Option");
@@ -72,29 +71,29 @@ void event_loop(int sock){
         new_sock = accept(sock, (struct sockaddr_in *)&client_addr, &sin_size); 
         if(new_sock < 0){
             perror("accept");
-            close(new_sock);
             continue;
         }
 
         printf("server: new connection from %s:%d\n", inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
         send(new_sock,  "Hello, there!\n", 13, 0);
-        recv_length = recv(new_sock, &buffer, 1024, 0);
+        recv_length = recv(new_sock, buffer, 1024, 0);
         while(recv_length > 0){
             printf("RECV: %d bytes\n", recv_length);
             dump(buffer, recv_length);
-            recv_length = recv(new_sock, &buffer, 1024, 0);
+            recv_length = recv(new_sock, buffer, 1024, 0);
         }
         close(new_sock);
     }
 }
 
 int main(void){
-    int sock = create_socket();
+    int sock;
+    create_socket(&sock);
     struct sockaddr_in host_addr, client_addr;
-    set_socket_option(sock);
+    set_socket_option(&sock);
     set_own_addr_information(&host_addr);
 
-    if(bind(sock, (struct sockaddr_in *)host_addr, sizeof(struct sockaddr)) < 0){
+    if(bind(sock, (struct sockaddr_in *)&host_addr, sizeof(struct sockaddr)) < 0){
         perror("bind");
         close(sock);
         return EXIT_FAILURE;
